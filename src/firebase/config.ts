2844717a62846dd71c16b9d76cdf26dc240e9b4f
Bolão Palmeiras <palmeirasbolao@gmail.com>
2025-10-04 00:38:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6599145879-d1474",
+  "appId": "1:380403707005:web:7b7f77c4f191c0b8aed514",
+  "apiKey": "AIzaSyC4PinPK1zq1T7BPuNFk1F8HY-um002KM4",
+  "authDomain": "studio-6599145879-d1474.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "380403707005"
+};
